be54681ef9b00a1d577b4771794aa8f9566378ba
Isimbi Nina <h.isimbi@alustudent.com>
2026-06-05 04:41:35 -0700

    Added execution logic

--- hospital_admin.sh
+++ hospital_admin.sh
@@ -31,12 +31,18 @@
   echo "System initialized" 
 
 }
-
- initialize_system 
  
 #Secures the active_logs directory by giving full access to the owner and displays the permissions
+
 secure_data() {
+
 	chmod 700 active_logs
+
 	ls -ld active_logs
 } 
 
+initialize_system
+secure_data
+
+echo "System Environment Secured"
+date
